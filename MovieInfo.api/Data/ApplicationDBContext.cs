1c9aff2cb7554f6d727b6aafb42fcbfbdb2f56aa
carlosapontelanda1@gmail.com <carlosapontelanda1@gmail.com>
2024-01-19 10:34:47 -0400

    Adding EFCore and SqlLite first changes

--- MovieInfo.api/Data/ApplicationDBContext.cs
+++ MovieInfo.api/Data/ApplicationDBContext.cs
@@ -4,5 +4,14 @@ namespace MovieInfo.api;
 
 public class ApplicationDBContext : DbContext
 {
+    public ApplicationDBContext(DbContextOptions dbContextOptions) 
+    : base(dbContextOptions)
+    {
+         
+    }
+
+    public DbSet<Movie> Movie { get; set; }
+    public DbSet<Actor> Actor { get; set; }
+    public DbSet<Director> Director { get; set; }
 
 }
